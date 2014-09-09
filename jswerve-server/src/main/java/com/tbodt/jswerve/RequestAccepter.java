db44a8daf759b2bd6669b513eb495f6bd6b6152f
Theodore Dubois <tblodt@icloud.com>
2014-09-09 13:20:41 -0700

    put this in a separate thread

---
+++ jswerve-server/src/main/java/com/tbodt/jswerve/RequestAccepter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2014 Theodore Dubois
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.tbodt.jswerve;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ * @author Theodore Dubois
+ */
+public class RequestAccepter implements Runnable {
+    private static Thread theThread;
+
+    public static void start() {
+        if (theThread == null)
+            theThread = new Thread(new RequestAccepter());
+        theThread.start();
+    }
+    
+    public static void stop() {
+        theThread.interrupt();
+        theThread = null;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            ExecutorService pool = Executors.newCachedThreadPool();
+            ServerSocket ss = new ServerSocket(Constants.PORT);
+            while (!Thread.interrupted()) {
+                Socket socket = ss.accept();
+                pool.execute(new RequestHandler(socket));
+            }
+        } catch (InterruptedIOException ex) {
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
