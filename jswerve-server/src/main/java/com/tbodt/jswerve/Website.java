ca095632b3a756515411c26804302fc21b6bc42d
Theodore Dubois <tblodt@icloud.com>
2014-09-11 17:18:16 -0700

    whoa, I think that'll work

--- jswerve-server/src/main/java/com/tbodt/jswerve/Website.java
+++ jswerve-server/src/main/java/com/tbodt/jswerve/Website.java
@@ -16,7 +16,12 @@
  */
 package com.tbodt.jswerve;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import java.io.*;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  *
@@ -27,39 +32,92 @@ public class Website {
     private static final File SITES = new File(JSwerver.HOME, "sites");
 
     private final String name;
-    private final File root;
-    private final Index index;
+    private final ZipFile archive;
+    private final Multimap<Request.Method, Page> entries = HashMultimap.create();
 
     public Website(String name) {
         this.name = name;
-        this.root = new File(SITES, name);
-    }
-
-    public final Response service(Request request) {
         try {
-            String path = request.getUri().getPath();
-            if (path.endsWith("/"))
-                path += "index.html";
-            String contentType = "text/plain";
-            if (path.lastIndexOf('.') != -1) {
-                String extension = path.substring(path.lastIndexOf('.') + 1);
-                if (contentTypes.containsKey(extension))
-                    contentType = contentTypes.get(extension);
-            }
+            this.archive = new ZipFile(new File(SITES, name + ".jar"));
+            ZipEntry mappings = archive.getEntry("META-INF/index");
+            BufferedReader mappingsReader = new BufferedReader(new InputStreamReader(archive.getInputStream(mappings)));
 
-            File file = new File(root, path);
-            if (!file.exists())
-                return new Response(StatusCode.NOT_FOUND);
-            InputStream pageIn = new FileInputStream(file);
-            ByteArrayOutputStream buf = new ByteArrayOutputStream();
-            int b;
-            while ((b = pageIn.read()) != -1)
-                buf.write(b);
-            return new Response(StatusCode.OK, buf.toByteArray(), contentType);
-        } catch (IOException e) {
-            throw new StatusCodeException(StatusCode.INTERNAL_SERVER_ERROR, e);
+            String requestPattern;
+            while ((requestPattern = mappingsReader.readLine()) != null) {
+                Request.Method method;
+                Pattern pattern;
+
+                String[] methodPattern = requestPattern.split(" ", 2);
+                ensure(methodPattern.length == 2);
+                method = Request.Method.forName(methodPattern[0]);
+                pattern = Pattern.compile(methodPattern[1]);
+
+                String response = mappingsReader.readLine();
+                ensure(response != null); // null means EOF
+                String[] typePath = requestPattern.split(":", 2);
+                ensure(typePath.length == 2);
+                String contentType = typePath[0];
+                String path = typePath[1];
+                entries.put(method, new StaticPage(pattern, path, contentType));
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
     }
+    
+    public Response service(Request request) {
+        String uri = request.getUri().toString();
+        Request.Method method = request.getMethod();
+        for (Page page : entries.get(method))
+            if (page.getPattern().matcher(uri).matches())
+                return page.serve(request);
+        return new Response(StatusCode.NOT_FOUND);
+    }
+
+    private static abstract class Page {
+        private final Pattern pattern;
+
+        public Page(Pattern pattern) {
+            this.pattern = pattern;
+        }
+
+        public Pattern getPattern() {
+            return pattern;
+        }
+
+        public abstract Response serve(Request request);
+    }
+
+    private final class StaticPage extends Page {
+        private final String path;
+        private final String contentType;
+
+        public StaticPage(Pattern pattern, String path, String contentType) {
+            super(pattern);
+            this.path = path;
+            this.contentType = contentType;
+        }
+
+        @Override
+        public Response serve(Request request) {
+            try {
+                ZipEntry entry = archive.getEntry(path);
+                InputStream pageIn = archive.getInputStream(entry);
+                ByteArrayOutputStream buf = new ByteArrayOutputStream();
+                int b;
+                while ((b = pageIn.read()) != -1)
+                    buf.write(b);
+                return new Response(StatusCode.OK, buf.toByteArray(), contentType);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+        public String getPath() {
+            return path;
+        }
+    }
+
 
     public static Website getCurrentWebsite() {
         return currentWebsite;
@@ -67,5 +125,10 @@ public class Website {
 
     public static void setCurrentWebsite(Website website) {
         currentWebsite = website;
+    }
+
+    private static void ensure(boolean condition) {
+        if (!condition)
+            throw new IllegalArgumentException("invalid syntax in index");
     }
 }
